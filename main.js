08597700585d5415a8c2b2e98aa0c1a358962dd0
Samuel Daza <sddaza.9@gmail.com>
2025-08-30 08:18:52 -0500

    Modificando sintaxis incorrecta

--- main.js
+++ main.js
@@ -6,5 +6,5 @@ saludo.addEventListener("click", () =>{
 
     const Nombre= document.querySelector(".recibirNombre").value;
     const apellido= document.querySelector(".recibirApellido").value;
-    alert(`Hola como estas ${Nombre} ${apellido}`)
+    alert(`Hola como estas ${Nombre} ${apellido}`);
 });
